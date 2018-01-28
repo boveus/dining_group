02d24a9948b67a7212533b0031c1db12f028c2bc
Brandon Stewart <boveus@gmail.com>
2018-01-28 13:03:06 -0500

    add logic to create groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,9 +2,21 @@ class Group < ApplicationRecord
   has_many :diners_groups
   has_many :diners, through: :diners_groups
 
-  def self.generate
-    new_members = Diner.new_members
-    not_new_members = Diner.not_new
+  def self.add_members_to_groups
+    new_members = Diner.new_members.to_a
+    not_new_members = Diner.not_new.to_a
+    new_members.shuffle
+    not_new_members.shuffle
+    groups = Group.all
+    counter = 0
+    while !(new_members.empty?) || !(not_new_members.empty?)
+      counter += 1
+      if !(new_members.empty?)
+        groups[counter % groups.count].diners << new_members.shift << not_new_members.shift
+      elsif !(not_new_members.empty?)
+        groups[counter % groups.count].diners << not_new_members.shift
+      end
+    end
   end
 
   def self.create_groups
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,6 @@ Diner.destroy_all
 Group.destroy_all
 DinersGroup.delete_all
 
-group_a = Group.create(letter: 'A')
-group_b = Group.create(letter: 'B')
-group_c = Group.create(letter: 'C')
-
-groups = [group_a, group_b, group_c]
-
 20.times do
   diner = Diner.create(
     name: "#{Faker::Name.first_name} #{Faker::Name.last_name}",
@@ -30,13 +24,4 @@ end
   )
 end
 
-
-# create_table :diners do |t|
-#   t.string :name
-#   t.string :email
-#   t.string :phone_number
-#   t.string :address
-#   t.string :group
-#
-#   t.timestamps
-# end
+Group.create_groups
