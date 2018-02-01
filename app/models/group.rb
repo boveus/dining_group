bede29596b8aaaaf546faa590d3ceed9096f82df
Brandon Stewart <boveus@gmail.com>
2018-02-01 11:08:28 -0500

    refactor groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,27 +2,31 @@ class Group < ApplicationRecord
   has_many :diners_groups
   has_many :diners, through: :diners_groups
 
+  DinerList = Struct.new(:new_ones, :not_new)
+
   def self.add_members_to_groups
-    new_members = Diner.new_members.to_a.shuffle
-    not_new_members = Diner.not_new.to_a.shuffle
-    groups = Group.all
     counter = 0
-    while !(new_members.empty?) || !(not_new_members.empty?)
-      if !(new_members.empty?)
-        groups[counter % groups.count].diners << new_members.shift << not_new_members.shift
-      elsif !(not_new_members.empty?)
-        groups[counter % groups.count].diners << not_new_members.shift
-      end
+    members = DinerList.new(Diner.new_members.to_a.shuffle, Diner.not_new.to_a.shuffle)
+    while !(members.new_ones.empty?) || !(members.not_new.empty?)
+      add_members(counter, members)
       counter += 1
     end
   end
 
-  def self.create_groups
-    group_count = (Diner.count / 10) + 1
-    alphabet_letters = ('A'..'Z').to_a
-
-    group_count.times do
-      Group.create(letter: alphabet_letters.shift)
+  def self.add_members(counter, members)
+    if !(members.new_ones.empty?)
+      Group.all[counter % Group.count].diners << members.new_ones.shift << members.not_new.shift
+    elsif !(members.not_new.empty?)
+      Group.all[counter % Group.count].diners << members.not_new.shift
     end
+  end
+
+  def self.number_of_groups
+     (Diner.count / 10) + 1
+  end
+
+  def self.create_groups
+    alphabet_letters = ('A'..'Z').to_a
+    number_of_groups.times { Group.create(letter: alphabet_letters.shift) }
   end
 end
